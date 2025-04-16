c62307cfb88b2264706d9f908eaa2fa05de60ff6
DarkPheonix29 <kealanthal29@gmail.com>
2025-04-16 13:30:28 +0200

    Add Availability and Booking entities to database schema
    This commit introduces two new entities, `Availability` and `Booking`, to the database schema. The `Availability` entity includes properties for `Id`, `InstructorEmail`, `Start`, and `End`, while the `Booking` entity includes properties for `Id`, `StudentEmail`, `InstructorEmail`, `Start`, and `End`. Additionally, a new column `Role` is added to the existing `Profile` table. Changes are reflected in the migration files and the model snapshot to ensure the database structure is updated accordingly.

---
+++ LBRepository/Migrations/20250416095637_BookingAndAvailability.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LBRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookingAndAvailability : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Profile",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Availability",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    InstructorEmail = table.Column<string>(type: "text", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Availability", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    StudentEmail = table.Column<string>(type: "text", nullable: false),
+                    InstructorEmail = table.Column<string>(type: "text", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Availability");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Profile");
+        }
+    }
+}
